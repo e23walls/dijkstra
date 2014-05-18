fe7bd2b404fdd3eacdc9bfdad3f3fe48dc0fac16
e23walls <emily_walls23@hotmail.com>
2014-05-18 14:25:25 -0400

    Initial Commit

---
+++ dijkstra/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  dijkstra
+//
+//  Created by Emily Jennyne Carroll Walls on 2014-05-18.
+//  Copyright (c) 2014 Emily Walls. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
